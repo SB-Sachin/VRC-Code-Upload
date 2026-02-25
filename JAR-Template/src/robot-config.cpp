7e7925cc69b379ad37e5587903e3173b9778dee6
ArvenSrikanth <arvensrikanth@gmail.com>
2026-02-25 17:16:45 -0500

    Update robot-config.cpp

--- JAR-Template/src/robot-config.cpp
+++ JAR-Template/src/robot-config.cpp
@@ -12,7 +12,17 @@ brain  Brain;
 
 //Add your devices below, and don't forget to do the same in robot-config.h:
 
-
+motor LeftFront = motor(PORT1, ratio6_1, true );
+motor LeftMiddle = motor(PORT2, ratio6_1, true);
+motor LeftBack = motor(PORT3, ratio6_1, true);
+motor rightFront = motor(PORT10, ratio6_1, false);
+motor rightMiddle = motor(PORT9, ratio6_1, false);
+motor rightBack = motor(PORT8, ratio6_1, false);  
+motor intake = motor(PORT13, ratio6_1, true);
+motor deposition = motor(PORT12, ratio6_1, true);
+digital_out toungePos = digital_out(Brain.ThreeWirePort.C);
+digital_out wingPneumatics = digital_out(Brain.ThreeWirePort.B);
+digital_out midgoalScorer = digital_out(Brain.ThreeWirePort.A);
 void vexcodeInit( void ) {
   // nothing to initialize
-}
+} 
